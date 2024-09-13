e82314f0a55f7861075882604d2eeb2029b68c9c
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2024-09-13 23:01:14 +0300

    add Layout comp and add some Text

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import "./App.css";
+import Layout from "./components/Layout";
 import Table from "./components/table";
 
 function App() {
-  return <Table />;
+  return (
+    <Layout>
+      <h1>Users table</h1>
+      <p>Here is the list of users!</p>
+      <p>You can sort users and filtering them! Let's do it!</p>
+      <Table />
+    </Layout>
+  );
 }
 
 export default App;
